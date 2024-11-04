56fd8ec1a2fbc45b39c6787d708aa1224c4366d4
ianimbuga <imbugaian20@gmail.com>
2024-11-04 22:23:54 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { image } from "../data/user"; 
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <p>I am a Web Developer from New York.</p>
+      <img src={image} alt="Profile" />
     </div>
   );
 }
+
+export default About;
+
+
+
+
